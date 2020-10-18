8095e40de7295ee67fee27fee97989f4d110fdbe
iaman <amankumar.kr95@gmail.com>
2020-10-18 07:30:05 +0530

    changes made in Singleton.java

--- src/com/amdocs/singleton/Singleton.java
+++ src/com/amdocs/singleton/Singleton.java
@@ -3,18 +3,17 @@ package com.amdocs.singleton;
 public class Singleton {
 	
 	private Singleton() {
-		//System.out.println(SingletonHelper.INSTANCE);
+		
 		System.out.println("in singleton constructor");
 	}
 	
 	private static class SingletonHelper{
-		private static int a=10;
-		//private static final Singleton INSTANCE = new Singleton();
+		
+		private static final Singleton INSTANCE = new Singleton();
 	}
 	
-	public static int getInstance() {
-		return SingletonHelper.a;
-	//	return SingletonHelper.INSTANCE;
+	public static Singleton getInstance() {
+		return SingletonHelper.INSTANCE;
 	}
 
 }
